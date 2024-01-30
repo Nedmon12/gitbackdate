81aacd296ecf56456ff8e95b4cfae5296f6f3d2e
Nedmon12 <mentesnot.ertibu@gmail.com>
2024-01-30 14:33:04 +0300

    year_month_date + months{literal} not compatible

--- main.cpp
+++ main.cpp
@@ -7,117 +7,120 @@
 #include <string>
 
 // do I need to define a struct?
-std::chrono::seconds
-to_unixepoch(std::chrono::time_point<std::chrono::system_clock> tp) {
-    return std::chrono::duration_cast<std::chrono::seconds>(tp);
-}
-class Codebase {
-  public:
-    Codebase() {
-        // parse info from ,git
-    }
-
-  private:
-    static std::string remote_address;
-
-  public:
-    void set_remote(std::string remote) { this->remote_address = remote; }
-    void push_to_remote() {
-        if (remote_address.length() == 0) {
-            std::cout << "remote address unspecified";
-            return;
-        }
-        system("git push -u origin");
-    }
-    void init() {
-        system("git init");
-        system("git branch -M main");
-    }
-};
-
-class Commit {
-  public:
-    Commit(std::string message, date::year_month_day commitDate,
-           std::string_view timezone, std::string hh_mm_ss) {
-        using namespace date;
-        this->message = message;
-        this->commitDate = commitDate;
-        // sprintf(cmd, "echo %s \n", echo);
-
-        sprintf(this->unix_timestamp, "%s %s %d %s %d", weekday{commitDate},
-                month{commitDate}, commitDate.day(), hh_mm_ss.c_str(),
-                static_cast<int>(commitDate.year()));
-
-        // weekday{commitDate}
-        // parse unit_timestamp here
-    }
-
-    std::string message;
-    // we only want to hold DD:MM:YYYY time will be randomized
-    // std::chrono::system_clock::time_point authorDate;
-
-    date::year_month_day commitDate;
-    const std::string timezone;
-    std::string ammended_date;
-    char *unix_timestamp;
-
-    // std::chrono:year_month_day a c++20 thing
-    // std::chrono::year_month_day
-    // commitDate{std::chrono::floor<std::chrono::days>(now)};
-    // std::chrono::system_clock::from_time_t(0);
-};
-// helpers
-bool cmdOptionExists(char **begin, char **end, const std::string &option) {
-    return std::find(begin, end, option) != end;
-}
-
-char *getCmdOption(char **begin, char **end, const std::string &option) {
-    char **itr = std::find(begin, end, option);
-    if (itr != end && ++itr != end) {
-        return *itr;
-    }
-    return 0;
-}
-// helpers
-
-// gitbackD options
-// duration
-// randomized -r
-// remote link
+// std::chrono::seconds
+// to_unixepoch(std::chrono::time_point<std::chrono::system_clock> tp) {
+//     return std::chrono::duration_cast<std::chrono::seconds>(tp);
+// }
+// class Codebase {
+//   public:
+//     Codebase() {
+//         // parse info from ,git
+//     }
 //
+//   private:
+//     static std::string remote_address;
 //
-
-void execute_command(std::string command) {
-    // why can't my linter understand
-    // cstdio system
-    int result = system(command);
-    if (result < 0) {
-        std::cout << "Erorr: Don't know what to do with yet";
-    }
-}
-
-void populate_timeline(std::string_view starter, std::string_view ender,
-                       int timezone, int commits) {
-    std::string commit_message = "artificially generated";
-    int counter = 1;
-    while (counter < commits) {
-        // Commit(std::string message, date::year_month_day commitDate,
-        // std::string_view timezone, std::string hh_mm_ss)
-        // Commit commit(commit_message,);
-    }
-}
+//   public:
+//     void set_remote(std::string remote) { this->remote_address = remote; }
+//     void push_to_remote() {
+//         if (remote_address.length() == 0) {
+//             std::cout << "remote address unspecified";
+//             return;
+//         }
+//         system("git push -u origin");
+//     }
+//     void init() {
+//         system("git init");
+//         system("git branch -M main");
+//     }
+// };
+//
+// class Commit {
+//   public:
+//     Commit(std::string message, date::year_month_day commitDate,
+//            std::string_view timezone, std::string hh_mm_ss) {
+//         using namespace date;
+//         this->message = message;
+//         this->commitDate = commitDate;
+//         // sprintf(cmd, "echo %s \n", echo);
+//
+//         sprintf(this->unix_timestamp, "%s %s %d %s %d", weekday{commitDate},
+//                 month{commitDate}, commitDate.day(), hh_mm_ss.c_str(),
+//                 static_cast<int>(commitDate.year()));
+//
+//         // weekday{commitDate}
+//         // parse unit_timestamp here
+//     }
+//
+//     std::string message;
+//     // we only want to hold DD:MM:YYYY time will be randomized
+//     // std::chrono::system_clock::time_point authorDate;
+//
+//     date::year_month_day commitDate;
+//     const std::string timezone;
+//     std::string ammended_date;
+//     char *unix_timestamp;
+//
+//     // std::chrono:year_month_day a c++20 thing
+//     // std::chrono::year_month_day
+//     // commitDate{std::chrono::floor<std::chrono::days>(now)};
+//     // std::chrono::system_clock::from_time_t(0);
+// };
+// // helpers
+// bool cmdOptionExists(char **begin, char **end, const std::string &option) {
+//     return std::find(begin, end, option) != end;
+// }
+//
+// char *getCmdOption(char **begin, char **end, const std::string &option) {
+//     char **itr = std::find(begin, end, option);
+//     if (itr != end && ++itr != end) {
+//         return *itr;
+//     }
+//     return 0;
+// }
+// // helpers
+//
+// // gitbackD options
+// // duration
+// // randomized -r
+// // remote link
+// //
+// //
+//
+// void execute_command(std::string command) {
+//     // why can't my linter understand
+//     // cstdio system
+//     int result = system(command);
+//     if (result < 0) {
+//         std::cout << "Erorr: Don't know what to do with yet";
+//     }
+// }
+//
+// void populate_timeline(std::string_view starter, std::string_view ender,
+//                        int timezone, int commits) {
+//     std::string commit_message = "artificially generated";
+//     int counter = 1;
+//     while (counter < commits) {
+//         // Commit(std::string message, date::year_month_day commitDate,
+//         // std::string_view timezone, std::string hh_mm_ss)
+//         // Commit commit(commit_message,);
+//     }
+// }
 
 int main(int argc, char *argv[]) {
-    // options
-    //-d [time duration]
-    //-r remote
-    //
-
-    char *echo = getCmdOption(argv, argv + argc, "-o");
-    if (echo) {
-        char cmd[100];
-
-        // This doesn't print to stdout right?
-        sprintf(cmd, "echo %s \n", echo);
+    std::int32_t commits = 999;
+    auto time_now = std::chrono::system_clock::now();
+    int yearsecs = 31556926;
+    auto today = date::year_month_day{
+        date::floor<date::days>(std::chrono::system_clock::now())};
+    auto last_year = today - date::years{1};
+    std::cout << "Last year was exactly on this date : " << last_year
+              << std::endl;
+    while (last_year < today) {
+        // make your commits here
+        last_year = last_year + date::months(1);
+        std::cout << "daily increment ?" << last_year << std::endl;
     }
+    // std::cout << "The timestamp right now is : " << time_now
+    // << std::endl;
 }
